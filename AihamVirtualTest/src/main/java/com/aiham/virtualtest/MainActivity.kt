66eadbce6af4151589034383cfc7140cfc6d80ea
edcvv77-lab <edcvv77@gmail.com>
2026-08-30 02:52:58 +0300

    test: log real guest activity resume

--- AihamVirtualTest/src/main/java/com/aiham/virtualtest/MainActivity.kt
+++ AihamVirtualTest/src/main/java/com/aiham/virtualtest/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.aiham.virtualtest
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "Guest MainActivity onCreate")
+
         val tv = TextView(this).apply {
             text = "Aiham Private Space Test"
             textSize = 24f
             gravity = android.view.Gravity.CENTER
         }
         setContentView(tv)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "Guest MainActivity onResume")
+    }
+
+    private companion object {
+        const val TAG = "AIHAM_GUEST_TEST"
     }
 }
